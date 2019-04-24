ca44b0b32eae85efd0646c6ce44accfa1f578074
zack <zack.etc@gmail.com>
2019-04-24 14:02:17 -0700

    node boiler

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+
+//----------------------------------MIDDLEWARE------------------------------//
+
+//parse urlencoded form data
+app.use(bodyParser.urlencoded({extended: true}));
+//parse json data
+app.use(bodyParser.json());
+
+//serve public directory
+app.use(express.static(__dirname + '/public'))
+
+
+
+//----------------------------------ROUTES------------------------------//
+
+
+// GET Root Route
+app.get('/', (req, res) => {
+    res.send(<h1>Auth App</h1>);
+});
+
+
+
+//----------------------------------START SERVER------------------------------//
+
+
+app.listen(PORT, () => {
+    console.log(`Sitting by the pool, but ready at port ${PORT} when you need me.`);
+});
